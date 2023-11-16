782cc90c68c13aff8e8e200ae441aa8ef60e2e86
jlobue10 <jlobue10@gmail.com>
2023-11-15 19:50:55 -0800

    Update install_config_from_GUI.sh

--- install_config_from_GUI.sh
+++ install_config_from_GUI.sh
@@ -5,6 +5,6 @@ ANS=$? 2>/dev/null
 if [[ $ANS == 0 ]]; then
     zenity --info --title="Success" --text="`printf "The refind.conf config file, OS icons and background image\nwere successfully moved to the refind folder on the /esp partition."`" --width=500 2>/dev/null
 else
-    zenity --error --title="Password Error" --text="`printf "Incorrect password provided, or some files were not found for installation.\nPlease try again providing the correct sudo password,\nand ensuring that the refind.conf config file, 4 OS icons, and background image\nexist in the /home/deck/.SteamDeck_rEFInd/GUI/ directory."`" --width=600 2>/dev/null
+    zenity --error --title="Password Error" --text="`printf "Incorrect password provided, or some files were not found for installation.\nPlease try again providing the correct sudo password,\nand ensuring that the refind.conf config file, 4 OS icons, and background image\nexist in the $HOME/.local/rEFInd_GUI/GUI/ directory."`" --width=600 2>/dev/null
     exit 1
 fi
