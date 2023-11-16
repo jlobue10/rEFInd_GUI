bc187e4c5283eb1f00fe48a0f9657d30aa6f2009
jlobue10 <jlobue10@gmail.com>
2023-11-15 19:08:23 -0800

    Update install-rEFInd-GUI.sh

--- install-rEFInd-GUI.sh
+++ install-rEFInd-GUI.sh
@@ -1,43 +1,19 @@
 #!/bin/bash
 # A simple script to install the rEFInd customization GUI
 
-echo ""
+echo -e "Installing rEFInd Customization GUI...\n"
+cd $HOME
+sudo rm -rf ./rEFInd_GUI
+git clone https://github.com/jlobue10/rEFInd_GUI
+cd rEFInd_GUI
+CURRENT_WD=$(pwd)
 
-read -p "Please make sure a sudo password is already set before continuing. If you have not set the user\
- or sudo password, please exit this installer with 'Ctrl+c' and then create a password either using 'passwd'\
- from a command line or by using the KDE Plasma User settings GUI. Otherwise, press Enter/Return to continue with the install."
-
-CURRENT_WD=$(pwd) 2>/dev/null
 mkdir -p $HOME/.local/rEFInd_GUI 2>/dev/null
-yes | cp -rf $CURRENT_WD/GUI/ $HOME/.local/rEFInd_GUI 2>/dev/null
-yes | cp -rf $CURRENT_WD/icons/ $HOME/.local/rEFInd_GUI 2>/dev/null
-yes | cp -rf $CURRENT_WD/backgrounds/ $HOME/.local/rEFInd_GUI 2>/dev/null
-
-# Create Install config from file to be used with passwordless sudo
-cat > $HOME/.local/rEFInd_GUI/install_config_from_GUI.sh <<EOF
-#!/bin/bash
-cp $HOME/.local/rEFInd_GUI/GUI/{refind.conf,background.png,os_icon1.png,os_icon2.png,os_icon3.png,os_icon4.png} /boot/efi/EFI/refind/ 2>/dev/null
-
-ANS=$? 2>/dev/null
-if [[ $ANS == 0 ]]; then
-    zenity --info --title="Success" --text="`printf "The refind.conf config file, OS icons and background image\nwere successfully moved to the refind folder on the /esp partition."`" --width=500 2>/dev/null
-else
-    zenity --error --title="Password Error" --text="`printf "Incorrect password provided, or some files were not found for installation.\nPlease try again providing the correct sudo password,\nand ensuring that the refind.conf config file, 4 OS icons, and background image\nexist in the /home/deck/.SteamDeck_rEFInd/GUI/ directory."`" --width=600 2>/dev/null
-    exit 1
-fi
-EOF
-
-chmod 555 $HOME/.local/rEFInd_GUI/install_config_from_GUI.sh 2>/dev/null
-
-#Create file for passwordless sudo for config file, background and icon installation
-cat > $HOME/.local/rEFInd_GUI/install_config_from_GUI <<EOF
-$USER ALL = NOPASSWD: $HOME/.local/rEFInd_GUI/install_config_from_GUI.sh
-EOF
-
-sudo cp $HOME/.local/rEFInd_GUI/install_config_from_GUI /etc/sudoers.d 2>/dev/null
-
-yes | cp $CURRENT_WD/{refind_install_package_mgr.sh,refind_install_Sourceforge.sh} $HOME/.local/rEFInd_GUI 2>/dev/null
-yes | cp $CURRENT_WD/refind-GUI.conf $HOME/.local/rEFInd_GUI/GUI/refind.conf 2>/dev/null
+cp -rf $CURRENT_WD/GUI/ $HOME/.local/rEFInd_GUI 2>/dev/null
+cp -rf $CURRENT_WD/icons/ $HOME/.local/rEFInd_GUI 2>/dev/null
+cp -rf $CURRENT_WD/backgrounds/ $HOME/.local/rEFInd_GUI 2>/dev/null
+cp -f $CURRENT_WD/{refind_install_package_mgr.sh,refind_install_Sourceforge.sh} $HOME/.local/rEFInd_GUI 2>/dev/null
+cp -f $CURRENT_WD/refind-GUI.conf $HOME/.local/rEFInd_GUI/GUI/refind.conf 2>/dev/null
 
 which dnf 2>/dev/null
 FEDORA_BASE=$?
@@ -52,6 +28,15 @@ if [ $FEDORA_BASE == 0 ]; then
   	else
    		sudo dnf install cmake hwinfo gcc-c++ qt6-qtbase-devel qt6-qttools-devel qt5-qtbase-devel qt5-qttools-devel xterm
      	fi
+	mkdir -p $HOME/rpmbuild/{SPECS,SOURCES}
+	cp rEFInd_GUI.spec $HOME/rpmbuild/SPECS
+	rpmbuild -bb $HOME/rpmbuild/SPECS/rEFInd_GUI.spec
+ 	sudo dnf list --installed | grep rEFInd_GUI
+  	REFIND_GUI_STATUS=$?
+   	if [ $REFIND_GUI_STATUS == 0 ]; then
+    		sudo dnf remove -y rEFInd_GUI
+	fi
+	sudo dnf install -y $HOME/rpmbuild/RPMS/x86_64/rEFInd_GUI*.rpm
 fi
 
 which apt 2>/dev/null
@@ -70,91 +55,15 @@ if [ $ARCH_BASE == 0 ]; then
 	#sudo pacman init and then pacman install build deps
 fi
 
-cd $HOME/.local/rEFInd_GUI/GUI/src 2>/dev/null
-ls -l $HOME/.local/rEFInd_GUI/GUI/src/build/CMakeCache.txt 2>/dev/null
-if [ $? == 0 ]; then
-	rm $HOME/.local/rEFInd_GUI/GUI/src/build/CMakeCache.txt 2>/dev/null
-fi
-ls -l $HOME/rEFInd_GUI/GUI/src/build/CMakeCache.txt 2>/dev/null
-if [ $? == 0 ]; then
-	rm $HOME/rEFInd_GUI/GUI/src/build/CMakeCache.txt 2>/dev/null
-fi
-mkdir -p build 2>/dev/null
-cd build 2>/dev/null
-cmake ..
-make
-
-if [ ! -f $HOME/.local/rEFInd_GUI/GUI/src/build/rEFInd_GUI ]; then
+if [ ! -f /usr/bin/rEFInd_GUI ]; then
 	echo -e "\nGUI compile failed. Please try again after ensuring that your cloned repo is up to date and your pacman config is normal.\n"
 	exit 1
 fi
-
-# Move compiled rEFInd_GUI binary into GUI folder
-cp rEFInd_GUI ../../ 2>/dev/null
 
 if [ $NOBARA == 0 ]; then
 	#fix packaging after compile (if necessary)
 	sudo dnf install gstreamer1-plugins-good-qt6 --allowerasing
 fi
 
-#Create .desktop icon entry. Needs cat with generic username
-cat > $HOME/.local/rEFInd_GUI/GUI/refind_GUI.desktop <<EOF
-[Desktop Entry]
-Categories=System;
-Comment=rEFInd Customization GUI
-Exec=$HOME/.local/rEFInd_GUI/GUI/rEFInd_GUI
-GenericName=rEFInd GUI
-Name=rEFInd GUI
-Path=$HOME/.local/rEFInd_GUI/GUI
-Icon=$HOME/.local/rEFInd_GUI/GUI/UEFI_icon.png
-StartupNotify=true
-Terminal=false
-Type=Application
-X-KDE-SubstituteUID=false
-EOF
-
+cp /usr/share/applications/rEFInd_GUI.desktop $HOME/.local/rEFInd_GUI/GUI/refind_GUI.desktop
 chmod +x $HOME/.local/rEFInd_GUI/GUI/refind_GUI.desktop 2>/dev/null
-
-while true; do
-	echo ""
-	read -p "Do you want to copy the rEFInd_GUI icon to the desktop? (y/n) " YN
-	case $YN in
-		[yY]) echo -e "\nOk, icon will be copied to the desktop.\n"
-			cp $HOME/.local/rEFInd_GUI/GUI/refind_GUI.desktop $HOME/Desktop
-			chmod +x $HOME/Desktop/refind_GUI.desktop
-			break;;
-		[nN]) echo -e "\nIcon will not be copied to the desktop.\n"
-			exit;;
-		*) echo -e "\nInvalid response.";;
-	esac
-done
-
-#Create script to pick random PNG background from backgrounds folder
-cat > $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh <<EOF
-#!/bin/bash
-RAND_BG=(lss $HOME/.local/rEFInd_GUI/backgrounds | grepp .png | shuff -n1) 2>/dev/null
-sudo cp $HOME/.local/rEFInd_GUI/backgrounds/USE_RAND_BG /boot/efi/EFI/refind/background.png 2>/dev/null
-EOF
-
-sed -i 's/(/"$(/g' $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh
-sed -i 's/)/)"/g' $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh
-sed -i 's/lss/ls/g' $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh
-sed -i 's/grepp/grep/g' $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh
-sed -i 's/shuff/shuf/g' $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh
-sed -i 's/USE_RAND_BG/$RAND_BG/g' $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh
-
-chmod +x $HOME/.local/rEFInd_GUI/*.sh 2>/dev/null
-
-#Create systemd service file for optional rEFInd background randomizer
-cat > $HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.service <<EOF
-[Unit]
-Description=Randomize Background on each rEFInd boot
-
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-ExecStart=/bin/bash -c '$HOME/.local/rEFInd_GUI/rEFInd_bg_randomizer.sh'
-
-[Install]
-WantedBy=multi-user.target
-EOF
