6ab1ef3d5387268f1f9caead77b3621ba1022261
jlobue10 <jlobue10@gmail.com>
2024-03-16 12:24:18 -0700

    Update install-rEFInd-GUI.sh

--- install-rEFInd-GUI.sh
+++ install-rEFInd-GUI.sh
@@ -81,10 +81,15 @@ sed -i "s@HOME@/home/$USER@g" $CURRENT_WD/rEFInd_GUI.desktop
 sed -i "s@HOME@/home/$USER@g" $CURRENT_WD/install_config_from_GUI.sh
 sed -i "s@USER@$USER@g" $CURRENT_WD/rEFInd_bg_randomizer.sh
 
+sudo mkdir -p /etc/rEFInd
 sudo cp -f $CURRENT_WD/install_config_from_GUI /etc/sudoers.d/install_config_from_GUI
-sudo cp -f $CURRENT_WD/install_config_from_GUI.sh /usr/bin/install_config_from_GUI.sh
+sudo cp -f $CURRENT_WD/install_config_from_GUI.sh /etc/rEFInd/install_config_from_GUI.sh
 sudo cp -f $CURRENT_WD/rEFInd_GUI.desktop /usr/share/applications/rEFInd_GUI.desktop
-sudo cp -f $CURRENT_WD/rEFInd_bg_randomizer.sh /usr/bin/rEFInd_bg_randomizer.sh
+sudo cp -f $CURRENT_WD/rEFInd_bg_randomizer.sh /etc/rEFInd/rEFInd_bg_randomizer.sh
+
+if [ $BAZZITE == 0 ]; then
+	sudo cp -f $CURRENT_WD/rEFInd_GUI.desktop /etc/rEFInd/rEFInd_GUI.desktop
+fi
 
 sudo chmod 777 /usr/bin/{install_config_from_GUI.sh,rEFInd_bg_randomizer.sh}
 
