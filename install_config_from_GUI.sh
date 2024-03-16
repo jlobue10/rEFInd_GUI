790f0e40a017304f80e679ae3b174d89f45524e1
jlobue10 <jlobue10@gmail.com>
2024-03-15 22:41:39 -0700

    Update install_config_from_GUI.sh

--- install_config_from_GUI.sh
+++ install_config_from_GUI.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cp $HOME/.local/rEFInd_GUI/GUI/{refind.conf,background.png,os_icon1.png,os_icon2.png,os_icon3.png,os_icon4.png} /boot/efi/EFI/refind/
+cp HOME/.local/rEFInd_GUI/GUI/{refind.conf,background.png,os_icon1.png,os_icon2.png,os_icon3.png,os_icon4.png} /boot/efi/EFI/refind/
