1a924052da0978f16519f80cbb3c74e685325c00
jlobue10 <jlobue10@gmail.com>
2024-03-15 17:50:43 -0700

    Update rEFInd_bg_randomizer.sh

--- rEFInd_bg_randomizer.sh
+++ rEFInd_bg_randomizer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-RAND_BG=$(ls /home/$USER/.local/rEFInd_GUI/backgrounds | grep .png | shuf -n1)
-sudo cp /home/$USER/.local/rEFInd_GUI/backgrounds/$RAND_BG /boot/efi/EFI/refind/background.png
+RAND_BG=$(ls /home/USER/.local/rEFInd_GUI/backgrounds | grep .png | shuf -n1)
+sudo cp /home/USER/.local/rEFInd_GUI/backgrounds/$RAND_BG /boot/efi/EFI/refind/background.png
