eb043da7d12c42fdd9afe5a0f126a2ac37d3fbdb
jlobue10 <jlobue10@gmail.com>
2024-03-16 21:33:20 -0700

    Update install-rEFInd-GUI.sh

--- install-rEFInd-GUI.sh
+++ install-rEFInd-GUI.sh
@@ -68,7 +68,7 @@ fi
 which pacman 2>/dev/null
 ARCH_BASE=$?
 
-if [ ! -f /usr/bin/rEFInd_GUI ] && [ $BAZZITE != 0 ]; then
+if [ ! -f /etc/rEFInd/rEFInd_GUI ] && [ $BAZZITE != 0 ]; then
 	echo -e "\nGUI compile failed. Please try again after ensuring that your cloned repo is up to date and your pacman config is normal.\n"
 	exit 1
 fi
