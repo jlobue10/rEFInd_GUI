49a60cb2aa047e13ced8c41fa18ef08e5d9506b1
jlobue10 <jlobue10@gmail.com>
2025-03-22 15:54:17 -0700

    Update mainwindow.cpp

--- GUI/src/mainwindow.cpp
+++ GUI/src/mainwindow.cpp
@@ -27,7 +27,7 @@ bool Boot_Last_OS_bool;
 bool Enable_Mouse_bool;
 bool Firmware_BootNum_bool;
 int Update_Num;
-int VERSION = 142;
+int VERSION = 143;
 ostringstream install_refind_apt_path_o;
 ostringstream install_refind_dnf_path_o;
 ostringstream install_refind_Sforge_path_o;
@@ -756,7 +756,7 @@ void MainWindow::on_About_pushButton_clicked()
     QPushButton* updateButton = new QPushButton("Check For Update");
     connect(updateButton, &QPushButton::clicked, this, &MainWindow::on_updateButton_Clicked);
     AboutBox.setTextFormat(Qt::RichText);
-    AboutBox.setText("<p align='center'>rEFInd Customization GUI v1.4.2<br><br>"
+    AboutBox.setText("<p align='center'>rEFInd Customization GUI v1.4.3<br><br>"
                      "Original GUI Creator: "
                      "<a href='https://github.com/jlobue10'>jlobue10</a><br><br>"
                      "Special Thanks to Deck Wizard for testing and QA"
