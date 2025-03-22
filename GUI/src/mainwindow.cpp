7726b05e31e0769bac59599db17794a43447d562
jlobue10 <jlobue10@gmail.com>
2025-03-22 15:23:31 -0700

    Update mainwindow.cpp

--- GUI/src/mainwindow.cpp
+++ GUI/src/mainwindow.cpp
@@ -481,6 +481,15 @@ string MainWindow::CreateBootStanza(QString &BootOption, const char *BootNum, bo
             Boot_Stanza_GUI.append("\tgraphics on\n}\n");
             return Boot_Stanza_GUI;
         }
+        if(Linux_Select_str == "CachyOS") {
+            Boot_Stanza_GUI.append("\nmenuentry \"CachyOS\" {\n");
+            Boot_Stanza_GUI.append("\ticon /EFI/refind/os_icon");
+            Boot_Stanza_GUI.append(BootNum);
+            Boot_Stanza_GUI.append(".png\n");
+            Boot_Stanza_GUI.append("\tloader /EFI/cachyos/grubx64.efi\n");
+            Boot_Stanza_GUI.append("\tgraphics on\n}\n");
+            return Boot_Stanza_GUI;
+        }
         if(Linux_Select_str == "CentOS") {
             Boot_Stanza_GUI.append("\nmenuentry \"CentOS\" {\n");
             Boot_Stanza_GUI.append("\ticon /EFI/refind/os_icon");
